57629bc30c645602f357cacbb2f7e10d356756ec
Harsh Tenguriya <thegreatharsh1@gmail.com>
2021-05-16 06:03:53 +0530

    Error Resolve

--- backend/app.js
+++ backend/app.js
@@ -33,10 +33,10 @@ app.get("/signup", (req, res) => {
 });
 
 if (process.env.NODE_ENV == "production") {
-  app.use(express.static("client/build"));
+  app.use(express.static("frontend/build"));
   const path = require("path");
   app.get("*", (req, res) => {
-    res.sendFile(path.resolved(__dirname, "client", "build", "index.html"));
+    res.sendFile(path.resolved(__dirname, "frontend", "build", "index.html"));
   });
 }
 
